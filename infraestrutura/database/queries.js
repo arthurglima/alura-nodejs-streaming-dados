541951c91f22d30225b2af38d27953e1dd7d00b4
Arthur Lima <arthur_lima_1@hotmail.com>
2021-03-29 12:46:18 -0300

    Adicionando  detalhe que faltou da aula 4

--- infraestrutura/database/queries.js
+++ infraestrutura/database/queries.js
@@ -2,11 +2,11 @@ const conexao = require('./conexao')
 
 const executaQuery = (query, parametros = '') => {
     return new Promise((resolve, reject) => {
-        conexao.query(query, parametros, () => {
+        conexao.query(query, parametros, (erros, resultados, campos) => {
             if(erro){
-    
+                reject(erros)
             } else {
-    
+                resolve(resultados)
             }
         })
     })
